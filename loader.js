831be1d5368549424a957fe8b9ab956780b5f905
GameBoo-x <abasfakre81@gmail.com>
2024-11-16 03:13:51 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,9 +1,11 @@
-const script = document.createElement('script');
-script.src = 'scripttt.js'; // المسار إلى الملف الرئيسي
-script.onload = () => {
-    console.log('Main script loaded successfully');
-};
-script.onerror = () => {
-    console.error('Failed to load the main script.');
-};
-document.head.appendChild(script);
+document.addEventListener('DOMContentLoaded', () => {
+    const script = document.createElement('script');
+    script.src = 'scripttt.js'; // المسار إلى الملف الرئيسي
+    script.onload = () => {
+        console.log('Main script loaded successfully');
+    };
+    script.onerror = () => {
+        console.error('Failed to load the main script.');
+    };
+    document.head.appendChild(script);
+});

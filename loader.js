6080b6566f411d35a8ca7eec69aad7d95ef0f8ef
GameBoo-x <abasfakre81@gmail.com>
2024-11-16 03:15:31 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,11 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
+// تحميل ملف JSON
+fetch('configs.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Failed to load config.json: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(config => {
+    // إنشاء عنصر script لتحميل الملف الرئيسي
     const script = document.createElement('script');
-    script.src = 'scripttt.js'; // المسار إلى الملف الرئيسي
+    script.src = config.mainScript; // المسار إلى ملف السكربت
     script.onload = () => {
-        console.log('Main script loaded successfully');
+      console.log('Main script loaded successfully');
     };
     script.onerror = () => {
-        console.error('Failed to load the main script.');
+      console.error('Failed to load the main script.');
     };
     document.head.appendChild(script);
-});
+  })
+  .catch(error => {
+    console.error('Error loading JSON or script:', error);
+  });
